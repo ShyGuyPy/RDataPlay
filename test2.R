c6fee7e4988f658c6456772efb489cf39f8f48dd
Luke Vawter <luke.vawter1@gmail.com>
2018-08-17 12:39:24 -0400

    playing with $extract to pull values by name from lists and such

--- test2.R
+++ test2.R
@@ -3,10 +3,23 @@ library(magrittr)
 my_data <-c(21,42,16,123)
 
 
+print("pipe output is: ")
 my_data %>%
   rev() %>%
   cat() 
  
+print("array value is still: ")
+cat( my_data)
+
+my_value = 13
+my_value %>%
+  as.character() %>%
+  typeof() %>%
+  cat()
+
+#as.character(my_value)
+
+#print(typeof(my_data))
   
    
   
