8096f8e54892f4e1dd3b3bacb6ddbb4f38163965
Luke Vawter <luke.vawter1@gmail.com>
2018-08-17 13:50:31 -0400

    playing with dataframes and dplyr

--- test2.R
+++ test2.R
@@ -1,39 +1,24 @@
 #this is a test
-library(magrittr, dplyr)
+library(dplyr)
 
-my_data <-c(21,42,16,123)
-my_data.df <- 
-
-
-print("pipe output is: ")
-my_data %>%
-  rev() %>%
-  cat() 
- 
-print("array value is still: ")
-cat( my_data)
-
-my_value = 13
-my_value %>%
-  as.character() %>%
-  typeof() %>%
-  cat()
-
-my_crazy_var <-list(a=3, b=10, apple=18)
-
-print(my_crazy_var$apple)
-muffin <- my_crazy_var$b + my_crazy_var$apple
-print(muffin)
-
-
-My_stuff.df <- data.frame(
+my_stuff.df <- data.frame(
   my_stuff_id = c(1:3),
   my_stuff_fruit = c("Orange", "plum", "mango"),
   amount = c(10, 24, 4),
+  time = c(103, 1400, 715),
   
   stringsAsFactors = FALSE
 )
-print(My_stuff.df)
+
+x <- data.frame("SN" = 1:2, "Age" = c(21,15), "Name" = c("John","Dora"))
+
+
+print(nrow(my_stuff.df))
+print(nrow(x))
+my_stuff_new.df <- my_stuff.df %>% 
+  dplyr::select(my_stuff_fruit)
+
+print(my_stuff_new.df)
   
 
   
